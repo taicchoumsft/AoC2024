5d83891ba7738103c795a092bb6eaa735c74ae8b
Tai Chou <tachou@microsoft.com>
2024-12-19 00:08:03 -0500

    day19

---
+++ day19/day19.py
@@ -0,0 +1,53 @@
+from functools import lru_cache
+import sys
+
+def solution1(patterns, arr):
+    def f(s):
+        if s == "":
+            return True
+
+        for p in patterns:
+            if s.startswith(p):
+                if f(s[len(p):]):
+                    return True
+        return False
+
+    total = 0
+    for a in arr:
+        if f(a):
+            total += 1
+    return total
+
+def solution2(patterns, arr):
+    @lru_cache(None)
+    def f(s):
+        if s == "":
+            return 1
+
+        total = 0
+        for p in patterns:
+            if s.startswith(p):
+                total += f(s[len(p):])
+        return total
+
+    total = 0
+    for a in arr:
+        total += f(a)
+    return total
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        raise ValueError("Please provide input file")
+
+    arr = []
+    patterns = None
+    with open(sys.argv[1], "r") as file:
+        patterns = file.readline().strip().split(", ")
+        file.readline()
+        for line in file:
+            arr += [line.strip()]
+
+    print("Solution 1: ", solution1(patterns, arr))
+    print("Solution 2: ", solution2(patterns, arr))
+
+
